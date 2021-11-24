473e0f57802a7cbd0f40f498b9609d8c8ed0d7ab
shifaanwar <anwarshifa580@gmail.com>
2021-11-24 20:06:09 +0800

    Create Prog2.c

---
+++ Week1/.vscode/Prog2.c
@@ -0,0 +1,13 @@
+// Question-2 From Week1
+
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter two integers\n");
+    scanf("%d %d",&num1,&num2);
+    if(num1>num2)
+    printf("%d is max",num1);
+    else
+    printf("%d is max",num2);
+}
