c35b3c60a5f67b567b788a1aec14201132e19174
shifaanwar <anwarshifa580@gmail.com>
2021-11-24 20:14:13 +0800

    Create Star.c

---
+++ Week3/Star.c
@@ -0,0 +1,22 @@
+//Ques-1 from Week3
+#include<stdio.h>
+void star(int);
+int main()
+{
+int no_of_rows;
+printf("Enter the number of stars");
+scanf("%d",& no_of_rows);
+star( no_of_rows);
+return 0;
+}
+void star(int n)
+{
+for(int row=1;row<=n;row++)
+{
+    for(int col=1;col<=row;col++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
+}
