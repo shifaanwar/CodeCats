310f31d61e46980b84387fa580e4601f3633366f
shifaanwar <anwarshifa580@gmail.com>
2021-11-30 22:09:03 +0800

    Create duck.c

---
+++ Week6/duck.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int checkDuck(char *,int);
+int main()
+{
+char s1[100];
+char *str = s1;
+printf("Enter the number:\n");
+scanf("%s",&s1);
+int length = strlen(s1); 
+int res=checkDuck(s1,length);
+if(res==1)
+printf("Duck Number");
+else
+printf("Not a Duck Number");
+}
+ int checkDuck(char n[],int length)
+{
+    int store=0;
+    if(n[0]=='0')
+     return 0;
+    for(int i=0;i<length;i++)
+    {
+        if(n[0]!='0' && n[i]=='0')
+           store = 1;
+    }
+    return store;
+}
