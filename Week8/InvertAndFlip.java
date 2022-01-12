a68bf7b47945385b3f183f1ae626a8962cfb143d
shifaanwar <anwarshifa580@gmail.com>
2022-01-12 23:32:56 +0800

    Create InvertAndFlip.java
    Invert And Flip Image

---
+++ Week8/InvertAndFlip.java
@@ -0,0 +1,28 @@
+class InvertAndFlip {
+    public int[][] flipAndInvertImage(int[][] image){
+    for(int row=0;row<image.length;row++)
+	{
+	 int left =0;
+	 int  right = image[0].length-1;
+	 while(left<right)
+     {
+	  int temp = image[row][left];
+	  image[row][left]=image[row][right];
+	  image[row][right]=temp;
+	  left++;
+	  right--;
+	  }
+	}
+ for(int i=0;i<image.length;i++)
+ {
+ for(int j=0;j<image[0].length;j++)
+ {
+ if(image[i][j]==0)
+ image[i][j]=1;
+ else
+ image[i][j]=0;
+ }
+ }
+  return image;      
+}
+}   
