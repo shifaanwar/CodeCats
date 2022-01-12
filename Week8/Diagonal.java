3543835fe73d4d21a5853f63eb2da81bf17fd8d0
shifaanwar <anwarshifa580@gmail.com>
2022-01-12 23:32:08 +0800

    Create Diagonal.java
    Sum Of Diagonals

---
+++ Week8/Diagonal.java
@@ -0,0 +1,19 @@
+class Diagonal
+{
+public int diagonalSum(int[][] mat) 
+{
+    int n = mat.length;
+    int sum =0;
+    for(int i=0;i<mat.length;i++)
+    {
+    for(int j=0;j<mat[0].length;j++)
+    {
+    if(i==j||j==n-i-1)
+    {
+    sum+=mat[i][j];
+    }
+    }
+    }
+    return sum;
+}
+}
